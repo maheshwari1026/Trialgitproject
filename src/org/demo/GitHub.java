375f0ae64f97654a4d80c985a876ab5d823a4f00
maheshwari1026 <146749774+maheshwari1026@users.noreply.github.com>
2023-10-07 14:02:48 +0530

    commit msg

---
+++ src/org/demo/GitHub.java
@@ -0,0 +1,13 @@
+package org.demo;
+
+public class GitHub {
+	private void Testcase1() {
+		System.out.println("Brower Opened");
+		
+
+	}
+public static void main(String[] args) {
+	GitHub s = new GitHub();
+	s.Testcase1();
+}
+} 
